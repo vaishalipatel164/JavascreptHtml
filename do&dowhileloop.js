0274328d3dec6fc1fc72f07df5b1abe8ef13a111
vaishalipatel164 <164patelvaishali@gmail.com>
2026-06-22 00:18:25 +0530

    do loop

---
+++ do&dowhileloop.js
@@ -0,0 +1,20 @@
+// print the number from 1 to 10 :
+
+// let a=1;
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
+// print the reverse number from 10 to 1 :
+ 
+// let b=10
+// while(b>=1){
+//     console.log(b);
+//     b--;
+// }
+
+let c=0;
+while(c<10){
+    console.log(c);
+    c++;
+}
