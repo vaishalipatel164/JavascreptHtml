318d2b1c04983a2d31151165d6f0f5b0933d9081
vaishalipatel164 <164patelvaishali@gmail.com>
2026-06-23 17:01:24 +0530

    learn do while loop

--- do&dowhileloop.js
+++ do&dowhileloop.js
@@ -1,14 +1,89 @@
+             // Using While Loop :-
+
 // print the number from 1 to 10 :
 
-let a=1;
-while(a<=10){
-    console.log(a);
-    a++;
-}
+// let a=1;
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
 // print the reverse number from 10 to 1 :
  
-let b=10;
-while(b>=1){
-    console.log(b);
-    b--;
-}
+// let b=10;
+// while(b>=1){
+//     console.log(b);
+//     b--;
+// }
+
+             // Using Do While Loop :-
+
+// print the number from 1 to 10:             
+// let i =1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 10);
+
+// print the reverse number from 10 t0 1:
+// let j=10;
+// do{
+//     console.log(j);
+//     j--;
+// }while(j>=1);
+
+// print the even number from 2 to 20:
+
+let k=2;
+// do{
+//     if(k%2==0){
+//      console.log(k);
+//     }
+//     k++;
+// }while(k<=20);
+
+// print the odd number from 1 to 30:
+
+// let l=1;
+// do{
+//     if(l%2==!0){
+//         console.log(l)
+//     }
+//       l++;
+// }while(l<=30);
+
+// find the sum of number from 1 to 5:
+
+// let m=1;let sum=0;
+// do{
+//     sum = sum + m;
+//     // console.log(sum);
+//     m++;   
+// }while(m<=5);
+//   console.log(sum);
+
+//  print the multiplication table of 5:  
+
+// let n=1;
+// do{
+//     console.log(n*5);
+//     n++;
+// }while(n<=10);
+
+// Print numbers divisible by 3 from 1 to 30:
+
+// let o=1;
+// do{
+//     if(o%3 == 0){
+//         console.log(o);
+//     }
+//     o++;
+// } while(o<=30)
+
+
+// Print squares of numbers from 1 to 5
+
+let p=1;
+ do{
+    console.log(p*p);
+    p++;
+ }while(p <= 5);
