4089e0be8e4e0fce283b30cdfaaacc12d3e84c52
vaishalipatel164 <164patelvaishali@gmail.com>
2026-06-26 01:03:34 +0530

    learn function

---
+++ functionexpression.js
@@ -0,0 +1,41 @@
+// find the square of number:
+
+function square(b){
+    return b*b;
+}
+console.log(square(6));
+
+const square = function(b){
+    return b*b;
+}
+ console.log(square(6));
+
+// check the even and odd number:
+
+// function num(i){
+//    if (i % 2 == 0){
+//     return "even";
+//    }
+//    else{
+//     return "odd";
+//    }
+//    return 0;
+// }
+// let check = num(14);
+// console.log(check);
+
+// find the maximum number:
+
+// function number(a,b){
+//     if(a>b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+//     return 0;
+// }
+// let max = number(1,9);
+// console.log(max);
+
+
