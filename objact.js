810bbc8c5263f1a0dfd7137c0a787a47ce5f4b0e
vaishalipatel164 <164patelvaishali@gmail.com>
2026-07-01 21:01:26 +0530

    learn This function

--- objact.js
+++ objact.js
@@ -6,11 +6,25 @@
 // }
 // document.getElementById("student").innerHTML=student.Name+" was"+" "+student.Qualification+" from"+" "+student.College+", "+student.Place+"."
 
-const resume = {
-    Name: ["Ashvi"],
-    Age: ["22"],
-    Skills: ["HTML"," CSS"," JavaScript"," and"," REACT.js"],
-    Experience: ["Fresher's"]
+// const resume = {
+//     Name: ["Ashvi"],
+//     Age: ["22"],
+//     Skills: ["HTML"," CSS"," JavaScript"," and"," REACT.js"],
+//     Experience: ["Fresher's"]
+// }
+// console.log(resume.Skills);
+// document.getElementById("resume").innerHTML=resume.Name+" is "+resume.Age+" year old, have some skills like "+resume.Skills+" and she is "+resume.Experience
+
+// console.log(resume);
+
+const person = {
+    firstname : "Vaishali",
+    lastname : "Patel",
+    age : "22",
+    skill : "Frontend Developer",
+    fullname: function (){
+        return this.firstname + " " + this.lastname +" is "+ this.skill;
+    }
 }
-console.log(resume.Skills);
-document.getElementById("resume").innerHTML=resume.Name+" is "+resume.Age+" year old, have some skills like "+resume.Skills+" and she is "+resume.Experience
+// console.log(person.fullname());
+document.getElementById("resume").innerHTML=person.fullname();  
